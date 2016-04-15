9e8c0a2f7bcb0b7cd92e0de5c622b6b297e558e3
Ilya Rostovtsev <ilyaforpresident@gmail.com>
2016-04-15 16:06:59 -0700

    data added

---
+++ colls/genData.py
@@ -0,0 +1,20 @@
+
+indices = range(0, 32)
+filenames = [str(i).zfill(2) + '.txt' for i in indices]
+
+writer = open('data.txt', 'w')
+
+for file in filenames:
+    f = open(file, 'r')
+    for entry in f:
+        nums = entry[entry.index(',') + 2 : entry.index(';')]
+        values = [float(num) for num in nums.split(' ')]
+        toWrite = ""
+        for i, v in enumerate(values):
+            toWrite += str(v)
+            if i < 2:
+                toWrite += ', '
+            else:
+                toWrite += '\n'
+        writer.write(toWrite)
+
