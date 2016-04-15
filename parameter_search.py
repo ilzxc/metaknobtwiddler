7797fd9f2702723a39a85fd2d9b257d0248db4ec
Rafael Valle <jrafaelvalle@gmail.com>
2016-04-15 15:33:17 -0700

    adding script to perform parameter search

---
+++ parameter_search.py
@@ -0,0 +1,46 @@
+"""Performs bayesian parameter optimization using parameters described in
+params.py file and data in inputted csv file"""
+from __future__ import division
+import os
+import argparse
+import numpy as np
+import neural_networks
+import bayesian_parameter_optimization as bpo
+from params import nnet_params, hyperparameter_space
+
+
+def set_trace():
+    from IPython.core.debugger import Pdb
+    import sys
+    Pdb(color_scheme='Linux').set_trace(sys._getframe().f_back)
+
+RESULTS_PATH = 'results/'
+
+if __name__ == '__main__':
+    # parse command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "csv_path", type=str,
+        help="Path to CSV file with distance(first col) and features")
+    args = parser.parse_args()
+
+    # Construct paths
+    trial_directory = os.path.join(RESULTS_PATH, 'parameter_trials')
+    model_directory = os.path.join(RESULTS_PATH, 'model')
+
+    model = 'general'
+    if model == 'general':
+        # Load data given csv file
+        data = np.loadtxt(args.csv_path, dtype=object, delimiter=",")
+
+        # Normalize data
+        data = (data - data.mean(axis=0)) / data.mean(axis=0)
+
+        # Run parameter optimization forever
+        bpo.parameter_search(data,
+                             nnet_params['general'],
+                             hyperparameter_space['general_network'],
+                             trial_directory,
+                             model_directory,
+                             neural_networks.train,
+                             model)
