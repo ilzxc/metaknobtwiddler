9e1c27a73e757ea89ea91ad8b31bda9853c32ac7
Rafael Valle <jrafaelvalle@gmail.com>
2016-04-15 15:53:16 -0700

    grabbing paths from params file

--- parameter_search.py
+++ parameter_search.py
@@ -1,20 +1,18 @@
 """Performs bayesian parameter optimization using parameters described in
 params.py file and data in inputted csv file"""
 from __future__ import division
-import os
 import argparse
 import numpy as np
 import neural_networks
 import bayesian_parameter_optimization as bpo
 from params import nnet_params, hyperparameter_space
+from params import TRIAL_DIRECTORY, MODEL_DIRECTORY
 
 
 def set_trace():
     from IPython.core.debugger import Pdb
     import sys
     Pdb(color_scheme='Linux').set_trace(sys._getframe().f_back)
-
-RESULTS_PATH = 'results/'
 
 if __name__ == '__main__':
     # parse command line arguments
@@ -23,10 +21,6 @@ if __name__ == '__main__':
         "csv_path", type=str,
         help="Path to CSV file with distance(first col) and features")
     args = parser.parse_args()
-
-    # Construct paths
-    trial_directory = os.path.join(RESULTS_PATH, 'parameter_trials')
-    model_directory = os.path.join(RESULTS_PATH, 'model')
 
     model = 'general'
     if model == 'general':
@@ -37,7 +31,7 @@ if __name__ == '__main__':
         bpo.parameter_search(data,
                              nnet_params['general'],
                              hyperparameter_space['general_network'],
-                             trial_directory,
-                             model_directory,
+                             TRIAL_DIRECTORY,
+                             MODEL_DIRECTORY,
                              neural_networks.train,
                              model)
